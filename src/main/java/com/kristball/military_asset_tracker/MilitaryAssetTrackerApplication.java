a7163c6244c9e3635a0be993354208789156b544
BUVANESHWAR S <buvansrinivasan@gmail.com>
2025-06-19 11:38:08 +0530

    Project Created

---
+++ src/main/java/com/kristball/military_asset_tracker/MilitaryAssetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kristball.military_asset_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitaryAssetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilitaryAssetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kristball/military_asset_tracker/MilitaryAssetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristball.military_asset_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryAssetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
